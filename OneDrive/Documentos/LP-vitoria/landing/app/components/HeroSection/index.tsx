289ea462772cf3d87bbe7078ac08cd6551c09148
Seu Nome <silvaflas021@gmail.com>
2025-11-17 12:07:32 -0300

    commit da construção da landing page

---
+++ OneDrive/Documentos/LP-vitoria/landing/app/components/HeroSection/index.tsx
@@ -0,0 +1,24 @@
+import type { Hero } from "@/app/types/heros/Hero"
+
+
+type Props = {
+    hero: Hero
+}
+
+
+
+const HeroSection = ({ hero }: Props) => {
+    return (
+        <section>
+            <div>
+                <h1>{hero.title}</h1>
+                <p>{hero.subtitle}</p>
+                <button>{hero.btnText}</button>
+            </div>
+        </section>
+
+    )
+
+}
+
+export default HeroSection
